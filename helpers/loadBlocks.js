cb232babcd6bac7dd55ef10a1c5a29af582f87a0
Felipe Lecot <lecotfelipe@gmail.com>
2024-08-01 12:43:04 -0300

    Create loadBlocks.js

---
+++ helpers/loadBlocks.js
@@ -0,0 +1,44 @@
+import { loadCarousel } from "./loadCarousel.js";
+import prisma from "./prisma.js"
+
+export const loadBlocks = async () => {
+    const blocks = await prisma.block.findMany({
+        orderBy: {
+            order: 'asc',
+        },
+        include: {
+            gallery: {
+                orderBy: {
+                    order: 'asc'
+                },
+                include: {
+                    image: true
+                }
+            }
+        }
+    });
+
+    let html = '';
+    for (const block of blocks) {
+        html += `
+            <div class="block ${block.color}" id="${block.slug}">
+                <h1>${block.heading}</h1>
+                ${block.content}
+
+                <div class="gallery">
+                    ${block.gallery.map(imageRelation => {
+                        return (`
+                            <figure>
+                                <img src="/assets/images/${imageRelation.image.name}" alt="${imageRelation.image.description}" width="200" height="150">
+                                <figcaption>${imageRelation.image.description}</figcaption>
+                            </figure>
+                        `)
+                }).join('')}
+                </div>
+                ${loadCarousel(block, block.gallery)}
+            </div>
+        `
+    }
+
+    return html;
+} 
