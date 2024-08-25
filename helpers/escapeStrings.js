e114a1dc688d5042fd680c1c96f6dcaf3f8f6391
Felipe Lecot <lecotfelipe@gmail.com>
2024-08-25 08:34:54 -0300

    remove useless comments

--- helpers/escapeStrings.js
+++ helpers/escapeStrings.js
@@ -1,10 +1,10 @@
 export const escapeStrings = (string) => {
     return string
-        .replace(/\\/g, '\\\\')   // Escape backslashes
-        .replace(/'/g, "\\'")     // Escape single quotes (JS strings)
-        .replace(/"/g, '&quot;')  // Escape double quotes (HTML attribute)
-        .replace(/</g, '&lt;')    // Escape <
-        .replace(/>/g, '&gt;')    // Escape >
-        .replace(/&/g, '&amp;')   // Escape &
-        .replace(/\n/g, '\\n');   // Optional: Escape newlines
+        .replace(/\\/g, '\\\\') 
+        .replace(/'/g, "\\'") 
+        .replace(/"/g, '&quot;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/&/g, '&amp;') 
+        .replace(/\n/g, '\\n'); 
 };
